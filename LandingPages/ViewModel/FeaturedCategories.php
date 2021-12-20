f0c727ca157bb11d3577bfd9f3f7c7afcc9c3d77
marcob-fisheye <85892511+marcob-fisheye@users.noreply.github.com>
2021-12-20 13:51:55 +0000

    Use Category List

--- LandingPages/ViewModel/FeaturedCategories.php
+++ LandingPages/ViewModel/FeaturedCategories.php
@@ -2,32 +2,47 @@
 
 namespace Marco\LandingPages\ViewModel;
 
-use Magento\Catalog\Model\ResourceModel\Category\Collection;
-use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory;
+use Magento\Catalog\Api\CategoryListInterface;
+use Magento\Catalog\Api\Data\CategorySearchResultsInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Api\SortOrder;
+use Magento\Framework\Api\SortOrderBuilder;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class FeaturedCategories implements ArgumentInterface
 {
-    protected CollectionFactory $categoryCollectionFactory;
+    protected CategoryListInterface $categoryList;
+    protected SearchCriteriaBuilder $searchCriteriaBuilder;
+    protected SortOrderBuilder $SortOrderBuilder;
 
-    public function __construct (
-        CollectionFactory $categoryCollectionFactory
-    )
-    {
-        $this->categoryCollectionFactory = $categoryCollectionFactory;
+    public function __construct(
+        CategoryListInterface $categoryList,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        SortOrderBuilder $SortOrderBuilder
+    ) {
+        $this->categoryList = $categoryList;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->sortOrderBuilder = $SortOrderBuilder;
     }
 
-    public function getCategories($level, $size, $sort, $order): Collection
+    /**
+     * @param int $level
+     * @param int $size
+     * @param string $order
+     */
+    public function getCategories($level, $size, $order): CategorySearchResultsInterface
     {
-        /** @var $categoryCollection Collection */
-        $categoryCollection = $this->categoryCollectionFactory->create();
+        $this->searchCriteriaBuilder->addFilter('level', $level);
+        $this->searchCriteriaBuilder->setPageSize($size);
 
-        $categoryCollection->addAttributeToFilter(
-            'level', $level
-        )
-        ->setPageSize($size)
-        ->setOrder($sort, $order);
+        $sortOrder = $this->sortOrderBuilder
+            ->setField($order)
+            ->setDirection(SortOrder::SORT_ASC)
+            ->create();
+        $this->searchCriteriaBuilder->addSortOrder($sortOrder);
 
-        return $categoryCollection;
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+       
+        return $this->categoryList->getList($searchCriteria);
     }
 }
